d0195a5b4683f437c3ae4af607c7b83f1a2164ec
QuentinGirard39 <girard.quentin39100512@gmail.com>
2022-01-03 15:03:41 +0100

    firstCommit

---
+++ src/app/administrateur/pied-page-admin/pied-page-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pied-page-client',
+  templateUrl: './pied-page-client.component.html',
+  styleUrls: ['./pied-page-client.component.css']
+})
+export class PiedPageClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
