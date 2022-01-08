c482fba348b530c3709643d9ce924e82fa46aa11
Guillaume Chevallier <51002806+GuitouNeoz@users.noreply.github.com>
2022-01-09 00:16:08 +0100

    Personnalisation de burger fonctionnelle

--- src/app/client/creation-burger/creation-burger.component.ts
+++ src/app/client/creation-burger/creation-burger.component.ts
@@ -14,11 +14,28 @@ export class CreationBurgerComponent implements OnInit {
   
 
   vueViande : boolean = false;
+  viandePouletSelected : boolean = false;
+  viandeSteackSelected : boolean = false;
+  viandePoissonSelected : boolean = false;
+  viandeVeggieSelected : boolean = false;
+
   vueSauce : boolean = false;
+  sauceKetchupSelected : boolean = false;
+  sauceBurgerSelected : boolean = false;
+  sauceMayoSelected : boolean = false;
+  sauceBiggieSelected: boolean = false;
+
+
   vueSupplements : boolean = false;
-  
+  suppBacconSelected : boolean = false;
+  suppCheddarSelected : boolean = false;
+  suppEmmentalSelected : boolean = false;
+  suppChevreSelected : boolean = false;
 
   countPain : number = 0;
+  countViande : number = 0;
+  countSauce : number = 0;
+  countSupp : number = 0;
   
   
   burger: any | undefined;
@@ -91,43 +108,139 @@ export class CreationBurgerComponent implements OnInit {
 
   }
 
-  annulerViande() :void{
-
-    this.vueViande = false;
-    this.vuePain = true;
-
-  }
+  
 
   choixViandePoulet() :void{
 
+    this.viandeSteackSelected = false;
+    this.viandePoissonSelected = false;
+    this.viandeVeggieSelected = false;
+
+    var otherBouton = document.getElementById("viandeSteack");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("viandePoisson");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("viandeVeggie");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("viandePoulet");
+    this.viandePouletSelected=true;
+
+    if(bouton && this.viandePouletSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
+
+    this.countViande = 1;
     this.burger["viande"] = "Poulet";
-    this.vueViande = false;
-    this.vueSauce = true;
     console.log(this.burger);
   }
 
   choixViandeSteack() :void{
 
+    this.viandePouletSelected = false;
+    this.viandePoissonSelected = false;
+    this.viandeVeggieSelected = false;
+
+    var otherBouton = document.getElementById("viandePoulet");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("viandePoisson");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("viandeVeggie");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("viandeSteack");
+    this.viandeSteackSelected=true;
+
+    if(bouton && this.viandeSteackSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
+
+    this.countViande = 1;
     this.burger["viande"] = "Steack";
-    this.vueViande = false;
-    this.vueSauce = true;
     console.log(this.burger);
   }
 
   choixViandePoisson() :void{
 
+    this.viandePouletSelected = false;
+    this.viandeSteackSelected = false;
+    this.viandeVeggieSelected = false;
+
+    var otherBouton = document.getElementById("viandePoulet");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("viandeSteack");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("viandeVeggie");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("viandePoisson");
+    this.viandePoissonSelected=true;
+
+    if(bouton && this.viandePoissonSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
+
+    this.countViande = 1;
     this.burger["viande"] = "Poisson";
-    this.vueViande = false;
-    this.vueSauce = true;
     console.log(this.burger);
     
   }
 
   choixViandeVeggie() :void{
 
+    this.viandePouletSelected = false;
+    this.viandeSteackSelected = false;
+    this.viandePoissonSelected = false;
+
+    var otherBouton = document.getElementById("viandePoulet");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("viandeSteack");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("viandePoisson");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("viandeVeggie");
+    this.viandeVeggieSelected=true;
+
+    if(bouton && this.viandeVeggieSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
+
+    this.countViande = 1;
+
     this.burger["viande"] = "Veggie";
-    this.vueViande = false;
-    this.vueSauce = true;
     console.log(this.burger);
     
   }
@@ -140,66 +253,327 @@ export class CreationBurgerComponent implements OnInit {
 
   }
 
+  annulerViande() :void{
+
+    this.vueViande = false;
+    this.vuePain = true;
+
+  }
+
 
 
 
 
   choixSauceKetchup() :void{
 
+    this.sauceBurgerSelected = false;
+    this.sauceMayoSelected = false;
+    this.sauceBiggieSelected = false;
+
+    var otherBouton = document.getElementById("sauceBurger");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("sauceMayo");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("sauceBiggie");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("sauceKetchup");
+    this.sauceKetchupSelected=true;
+
+    if(bouton && this.sauceKetchupSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
+
+    this.countSauce = 1;
+
     this.burger["sauce"] = "Ketchup";
-    this.vueSauce = false;
-    this.vueSupplements = true;
     console.log(this.burger);
   }
 
   choixSauceBurger() :void{
 
+    this.sauceKetchupSelected = false;
+    this.sauceMayoSelected = false;
+    this.sauceBiggieSelected = false;
+
+    var otherBouton = document.getElementById("sauceKetchup");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("sauceMayo");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("sauceBiggie");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("sauceBurger");
+    this.sauceBurgerSelected=true;
+
+    if(bouton && this.sauceBurgerSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
+
+    this.countSauce = 1;
+
     this.burger["sauce"] = "Burger";
-    this.vueSauce = false;
-    this.vueSupplements = true;
     console.log(this.burger);
   }
 
   choixSauceMayo() :void{
 
+    this.sauceBurgerSelected = false;
+    this.sauceKetchupSelected = false;
+    this.sauceBiggieSelected = false;
+
+    var otherBouton = document.getElementById("sauceBurger");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("sauceKetchup");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("sauceBiggie");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("sauceMayo");
+    this.sauceMayoSelected=true;
+
+    if(bouton && this.sauceMayoSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
+
+    this.countSauce = 1;
+
     this.burger["sauce"] = "Mayonnaise";
-    this.vueSauce = false;
-    this.vueSupplements = true;
     console.log(this.burger);
   }
 
   choixSauceBiggie() :void{
 
+    this.sauceBurgerSelected = false;
+    this.sauceKetchupSelected = false;
+    this.sauceMayoSelected = false;
+
+    var otherBouton = document.getElementById("sauceBurger");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("sauceKetchup");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("sauceMayo");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("sauceBiggie");
+    this.sauceBiggieSelected=true;
+
+    if(bouton && this.sauceBiggieSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
+
+    this.countSauce = 1;
+    
+
     this.burger["sauce"] = "Biggie";
-    this.vueSauce = false;
-    this.vueSupplements = true;
     console.log(this.burger);
   }
 
+
+
+
+  selectSauce() : void{
+    this.vueSauce = false;
+    this.vueSupplements = true;
+  }
+
+  annulerSauce() : void {
+
+    this.vueSauce = false;
+    this.vueViande = true;
+    
+  }
+
+
+
+
+
+
+
+
   choixSuppBaccon() :void{
 
+    this.suppCheddarSelected = false;
+    this.suppEmmentalSelected = false;
+    this.suppChevreSelected = false;
+
+    var otherBouton = document.getElementById("suppCheddar");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("suppEmmental");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("suppChevre");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("suppBaccon");
+    this.suppBacconSelected=true;
+
+    if(bouton && this.suppBacconSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
+
+    this.countSupp = 1;
+
     this.burger["supplément"]="Baccon";
-    
     console.log(this.burger);
 
   }
 
   choixSuppCheddar() :void{
+    this.suppBacconSelected = false;
+    this.suppEmmentalSelected = false;
+    this.suppChevreSelected = false;
+
+    var otherBouton = document.getElementById("suppBaccon");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("suppEmmental");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("suppChevre");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("suppCheddar");
+    this.suppCheddarSelected=true;
+
+    if(bouton && this.suppCheddarSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
 
 
+    this.countSupp = 1;
+
+
+    this.burger["supplément"]="Cheddar";
+    console.log(this.burger);
   }
 
   choixSuppEmmental() :void{
 
+    this.suppCheddarSelected = false;
+    this.suppBacconSelected = false;
+    this.suppChevreSelected = false;
 
+    var otherBouton = document.getElementById("suppCheddar");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("suppBaccon");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("suppChevre");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("suppEmmental");
+    this.suppEmmentalSelected=true;
+
+    if(bouton && this.suppEmmentalSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
+
+    this.countSupp = 1;
+
+
+
+    this.burger["supplément"]="Emmental";
+    console.log(this.burger);
   }
 
   choixSuppChevre() :void{
 
+    this.suppCheddarSelected = false;
+    this.suppBacconSelected = false;
+    this.suppEmmentalSelected = false;
 
+    var otherBouton = document.getElementById("suppCheddar");
+    if(otherBouton){
+      otherBouton.style.borderStyle="none";
+    }
+    var otherBouton1 = document.getElementById("suppBaccon");
+    if(otherBouton1){
+      otherBouton1.style.borderStyle="none";
+    }
+    var otherBouton2 = document.getElementById("suppEmmental");
+    if(otherBouton2){
+      otherBouton2.style.borderStyle="none";
+    }
+
+    var bouton = document.getElementById("suppChevre");
+    this.suppChevreSelected=true;
+
+    if(bouton && this.suppChevreSelected == true){
+      bouton.style.borderStyle="solid";
+      bouton.style.borderColor="blue";
+      bouton.style.borderWidth="3px";
+    }
+
+    this.countSupp = 1;
+
+
+    this.burger["supplément"]="Chevre";
+    console.log(this.burger);
   }
 
 
+  selectSupp() : void{
+    
+  }
+
+  annulerSupp() : void {
+    this.vueSauce = true;
+    this.vueSupplements = false;
+    
+  }
 
   
 
